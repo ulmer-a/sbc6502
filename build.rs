6dcc1eed207ec02fd8ed88149ec2a8f586eb4d73
Alexander Ulmer <ulmer@student.tugraz.at>
2022-06-10 16:19:19 +0200

    add custom board builder

--- build.rs
+++ build.rs
@@ -5,6 +5,6 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=emulator");
-    println!("cargo:rustc-link-lib=static=stdc++");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
 }
 
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod emulator;
+mod custom_board;
+
+pub use emulator::{Emulator, MemoryBus};
+pub use custom_board::{CustomBoard};
